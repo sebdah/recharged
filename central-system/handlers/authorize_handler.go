bc66ee04b03184b3ed10d6985adfa56632a61e77
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-29 16:19:29 +0200

    Removed debug code

--- central-system/handlers/authorize_handler.go
+++ central-system/handlers/authorize_handler.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/awslabs/aws-sdk-go/gen/dynamodb"
@@ -13,6 +12,4 @@ var Db *dynamodb.DynamoDB = database.GetDb()
 func AuthorizeHandler(rw http.ResponseWriter, r *http.Request) {
 	//token := idtag.NewIdToken(mux.Vars(r)["id"])
 	//tag := idtag.NewIdTag(token)
-
-	fmt.Println(Db.ListTables(new(dynamodb.ListTablesInput)))
 }
