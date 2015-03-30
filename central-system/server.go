4c599511daae02a6bb8aead087deda7a9246c47b
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-30 13:57:26 +0200

    Creating tables on start

--- central-system/server.go
+++ central-system/server.go
@@ -5,13 +5,29 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/sebdah/recharged/central-system/database"
 	"github.com/sebdah/recharged/central-system/routers"
 )
 
 func main() {
+	// Set the environment
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "dev"
+	}
+	fmt.Printf("Using environment '%s'\n", env)
+
+	// Set default port
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
+	}
+
+	// Create databases if needed
+	if env == "dev" {
+		fmt.Println("Ensuring databases")
+		tableIdTagWhitelist := database.TableIdTagWhitelist()
+		tableIdTagWhitelist.EnsureTable()
 	}
 
 	fmt.Printf("Starting webserver on port %s\n", port)
