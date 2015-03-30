1fac2257695e54778ca90088e083d2e83de67575
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-30 10:29:19 +0200

    Restructured the database package

---
+++ central-system/database/table.go
@@ -0,0 +1,136 @@
+package database
+
+import (
+	"log"
+
+	"github.com/awslabs/aws-sdk-go/service/dynamodb"
+)
+
+type Table struct {
+	TableName          string
+	ReadCapacityUnits  int64
+	WriteCapacityUnits int64
+	HashKeyName        string
+	HashKeyType        string
+	RangeKeyName       string
+	RangeKeyType       string
+}
+
+// Create table
+func (table *Table) CreateTable() (output *dynamodb.CreateTableOutput, err error) {
+	input := table.GetCreateTableInput()
+	output, err = Db.CreateTable(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+// Delete table
+func (table *Table) DeleteTable() (output *dynamodb.DeleteTableOutput, err error) {
+	input := table.GetDeleteTableInput()
+	output, err = Db.DeleteTable(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+// Ensure table
+func (table *Table) EnsureTable() {
+	tables, err := Db.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Skip table creation if the table already exists
+	for i := range tables.TableNames {
+		if tables.TableNames[i] == &table.TableName {
+			return
+		}
+	}
+
+	// Create the table
+	table.CreateTable()
+	return
+}
+
+// Define the table for creation
+func (table *Table) GetCreateTableInput() *dynamodb.CreateTableInput {
+	var (
+		hashKeyType  string = "HASH"
+		rangeKeyType string = "RANGE"
+	)
+
+	// Create provisioning settings
+	provisionedThroughput := dynamodb.ProvisionedThroughput{
+		ReadCapacityUnits:  &table.ReadCapacityUnits,
+		WriteCapacityUnits: &table.WriteCapacityUnits,
+	}
+
+	// Generate attribute definitions
+	var attributeDefinitions []*dynamodb.AttributeDefinition
+	hashAttributeDefinition := &dynamodb.AttributeDefinition{
+		AttributeName: &table.HashKeyName,
+		AttributeType: &table.HashKeyType,
+	}
+	attributeDefinitions = append(attributeDefinitions, hashAttributeDefinition)
+
+	if table.RangeKeyName != "" {
+		rangeAttributeDefinition := &dynamodb.AttributeDefinition{
+			AttributeName: &table.RangeKeyName,
+			AttributeType: &table.RangeKeyType,
+		}
+		attributeDefinitions = append(attributeDefinitions, rangeAttributeDefinition)
+	}
+
+	// Generate key schema
+	var keySchema []*dynamodb.KeySchemaElement
+	hashKey := &dynamodb.KeySchemaElement{
+		AttributeName: &table.HashKeyName,
+		KeyType:       &hashKeyType,
+	}
+	keySchema = append(keySchema, hashKey)
+	if table.RangeKeyName != "" {
+		rangeKey := &dynamodb.KeySchemaElement{
+			AttributeName: &table.RangeKeyName,
+			KeyType:       &rangeKeyType,
+		}
+		keySchema = append(keySchema, rangeKey)
+	}
+
+	return &dynamodb.CreateTableInput{
+		AttributeDefinitions:  attributeDefinitions,
+		KeySchema:             keySchema,
+		ProvisionedThroughput: &provisionedThroughput,
+		TableName:             &table.TableName,
+	}
+}
+
+// Define the table for deletion
+func (table *Table) GetDeleteTableInput() (input *dynamodb.DeleteTableInput) {
+	input = &dynamodb.DeleteTableInput{
+		TableName: &table.TableName,
+	}
+
+	return
+}
+
+// Recreate the table (or just create it if it doesn't exist)
+func (table *Table) RecreateTable() {
+	tables, err := Db.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete table if the table already exists
+	for i := range tables.TableNames {
+		if tables.TableNames[i] == &table.TableName {
+			table.DeleteTable()
+		}
+	}
+
+	// Create the table
+	table.CreateTable()
+	return
+}
--- central-system/database/table_idtag_whitelist.go
+++ central-system/database/table_idtag_whitelist.go
@@ -1,43 +1,12 @@
 package database
 
-import (
-	"github.com/awslabs/aws-sdk-go/service/dynamodb"
-)
+func TableInputIdTagWhitelist() *Table {
+	table := new(Table)
+	table.TableName = "idtag_whitelist"
+	table.ReadCapacityUnits = 1
+	table.WriteCapacityUnits = 1
+	table.HashKeyName = "idtag"
+	table.HashKeyType = "S"
 
-func TableInputIdTagWhitelist() dynamodb.CreateTableInput {
-	var (
-		tableName     string = "idtag_whitelist"
-		idTagName     string = "idtag"
-		idTagType     string = "S"
-		idTagKeyType  string = "HASH"
-		readCapacity  int64  = 1
-		writeCapacity int64  = 1
-	)
-
-	// Set provisioning
-	provisionedThroughput := dynamodb.ProvisionedThroughput{
-		ReadCapacityUnits:  &readCapacity,
-		WriteCapacityUnits: &writeCapacity,
-	}
-
-	// Create list of attribute definitions
-	var attributeDefinitions []*dynamodb.AttributeDefinition
-	attributeDefinitions = append(attributeDefinitions, &dynamodb.AttributeDefinition{
-		AttributeName: &idTagName,
-		AttributeType: &idTagType,
-	})
-
-	// Create list of keys
-	var keySchema []*dynamodb.KeySchemaElement
-	keySchema = append(keySchema, &dynamodb.KeySchemaElement{
-		AttributeName: &idTagName,
-		KeyType:       &idTagKeyType,
-	})
-
-	return dynamodb.CreateTableInput{
-		AttributeDefinitions:  attributeDefinitions,
-		KeySchema:             keySchema,
-		ProvisionedThroughput: &provisionedThroughput,
-		TableName:             &tableName,
-	}
+	return table
 }
