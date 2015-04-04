117a8c8ca691217be527339236bd13de9a186ea1
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-04 08:01:00 +0200

    Added Save() method for IdTags

--- central-system/database/collection_id_tags.go
+++ central-system/database/collection_id_tags.go
@@ -2,12 +2,19 @@ package database
 
 import "gopkg.in/mgo.v2"
 
+var collectionName string = "idTags"
+
 func CreateCollectionIdTags() {
 	database := GetDb()
 	collection := mgo.Collection{
 		Database: database,
-		Name:     "idTags",
-		FullName: database.Name + ".idTags",
+		Name:     collectionName,
+		FullName: database.Name + "." + collectionName,
 	}
 	collection.Create(&mgo.CollectionInfo{})
+}
+
+func GetCollectionIdTags() *mgo.Collection {
+	database := GetDb()
+	return database.C(collectionName)
 }
--- central-system/models/id_tag.go
+++ central-system/models/id_tag.go
@@ -1,26 +1,48 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/sebdah/recharged/central-system/database"
 	"github.com/sebdah/recharged/central-system/types"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-var table Table = database.TableIdTags()
+var collection *mgo.Collection
 
 type IdTag struct {
-	Id         string `type:"string" required:"true"`
-	IdType     string `type:"string" required:"true" default:"ISO14443"`
-	ExpiryDate string `type:"string" required:"false"`
-	GroupIdTag string `type:"string" required:"false"`
-	Language   string `type:"string" required:"false" default:"en"`
-	Active     bool   `type:"bool" required:"true" default:"false"`
+	Id         bson.ObjectId `bson:"_id,omitempty"`
+	IdTag      string        `type:"string" required:"true"`
+	IdType     string        `type:"string" required:"true" default:"ISO14443"`
+	ExpiryDate string        `type:"string" required:"false"`
+	GroupIdTag string        `type:"string" required:"false"`
+	Language   string        `type:"string" required:"false" default:"en"`
+	Active     bool          `type:"bool" required:"true" default:"false"`
 }
 
 // Constructor
 func NewIdTag(id string) (idTag *IdTag) {
-	idTag.Id = id
-	idTag.idType = types.IdTypeISO14443
+	idTag = new(IdTag)
+	idTag.IdTag = id
+	idTag.IdType = types.IdTypeISO14443
 	idTag.Language = "en"
 	idTag.Active = false
+
+	collection = database.GetCollectionIdTags()
+
 	return
+}
+
+// Save the document
+func (idTag *IdTag) Save() *IdTag {
+	fmt.Printf("Id: %s\n", idTag.Id)
+	if idTag.Id == "" {
+		idTag.Id = bson.NewObjectId()
+		collection.Insert(&idTag)
+	} else {
+		collection.Update(bson.M{"_id": idTag.Id}, &idTag)
+	}
+
+	return idTag
 }
