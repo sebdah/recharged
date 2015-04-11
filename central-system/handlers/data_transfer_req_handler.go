c53f1568b776f392989116b3598d19788c6c2f32
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-11 11:20:12 +0200

    Wrote handler for DataTransfer.Req #29

---
+++ central-system/handlers/data_transfer_req_handler.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/sebdah/recharged/central-system/messages"
+	"github.com/sebdah/recharged/central-system/types"
+)
+
+func DataTransferReqHander(w http.ResponseWriter, r *http.Response) {
+	// Populate the response configuration
+	conf := new(messages.DataTransferConf)
+	conf.Status = types.DataTransferStatusUnknownVendorId
+	js, err := json.Marshal(conf)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+
+	// Respond
+	w.WriteHeader(http.StatusOK)
+	w.Write(js)
+}
