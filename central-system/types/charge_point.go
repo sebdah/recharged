ee0357eb1b1af259a050d6886cd7c813382beddf
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-11 16:14:02 +0200

    Removed ChargePoint type, implemented as a model

--- central-system/types/charge_point.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type ChargePoint struct {
-	model        string `max_length:20`
-	serialNumber string `max_length:25`
-	vendor       string `max_length:20`
-	evses        []*Evse
-}
