df55d019bf1698b259d529cd89e193b76ce79d62
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-11 11:19:41 +0200

    Fixed typo #29

--- central-system/types/data_transfer_status.go
+++ central-system/types/data_transfer_status.go
@@ -1,8 +1,8 @@
 package types
 
 const (
-	DataTransferStatusAccepted                 string = "Accepted"
-	DataTransferStatusRejected                 string = "Rejected"
-	DataTransferStatusUnknownMessageId         string = "UnknownMessageId"
-	VendorIdDataTransferStatusUnknowenVendorId string = "UnknownVendorId"
+	DataTransferStatusAccepted         string = "Accepted"
+	DataTransferStatusRejected         string = "Rejected"
+	DataTransferStatusUnknownMessageId string = "UnknownMessageId"
+	DataTransferStatusUnknownVendorId  string = "UnknownVendorId"
 )
