6ccf6fee4ab658edb319629af1ce114ff097b19e
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-22 09:22:06 +0200

    WIP Implementing Authorize tests

---
+++ ws-test-client/messages/authorize.go
@@ -0,0 +1,12 @@
+package messages
+
+import (
+	"testing"
+
+	"github.com/sebdah/recharged/central-system/rpc"
+)
+
+func TestAuthorizeBasic(t *testing.T) {
+	// Create call object
+	call := rpc.NewCall()
+}
