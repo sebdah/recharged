ff132b513166e7c94ff3bb59664fcff7e48c9d2d
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-04-22 09:18:23 +0200

    Wrote initial unit testing for the Authorize messages

--- central-system/messages/authorize_test.go
+++ central-system/messages/authorize_test.go
@@ -3,8 +3,15 @@ package messages
 import (
 	"testing"
 
+	"github.com/sebdah/recharged/central-system/models"
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	idTag := models.NewIdTag()
+	models.DropCollection(idTag)
+	models.EnsureIndexes(idTag)
+}
 
 // Test simple JSON parsing
 func TestAuthorizeReqParseJson(t *testing.T) {
@@ -22,3 +29,14 @@ func TestAuthorizeReqString(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, `{"idTag":{"id":"1234","idType":"ISO14443"}}`, req.String())
 }
+
+// WIP - Test processing of missing IdTag
+//func TestAuthorizeReqProcessMissingIdTag(t *testing.T) {
+//// Create the request
+//req := new(AuthorizeReq)
+//err := req.ParseJson(`{"idTag": { "id": "11234fsd", "idType": "ISO14443" } }`)
+//assert.Nil(t, err)
+
+//_, errorer := req.Process()
+//assert.Nil(t, errorer)
+//}
